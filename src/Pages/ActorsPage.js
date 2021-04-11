331a6d878b5bc5d97a24bb1c82bd056e73bcddff
Zehavit <levi.zehavit@gmail.com>
2021-04-11 14:30:25 +0300

    clean new project add actorsPage

---
+++ src/Pages/ActorsPage.js
@@ -0,0 +1,10 @@
+function ActorsPage(){
+    const [actors, setActors] = React.useState();
+    const [filterText, setFiltaerTexe] = React.useState();
+    const [sortBy, setSortBy] = React.useState();
+    return(
+        <div></div>
+    );
+}
+
+export default ActorsPage;
