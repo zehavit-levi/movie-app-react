5a589fa20676dfebc23ebf523ca17f8bfce45979
Zehavit <levi.zehavit@gmail.com>
2021-04-14 18:44:24 +0300

    send prop sortBy and filterBy to ActorsCards and render refreshed actors list

--- src/Pages/ActorsPage.js
+++ src/Pages/ActorsPage.js
@@ -8,25 +8,26 @@ import ActorModel from "../model/ActorModel";
 function ActorsPage() {
   const [actors, setActors] = useState(null);
   const [filterText, setFilterText] = useState("");
-  const [sortBy, setSortBy] = useState("0");
+  const [sortBy, setSortBy] = useState("firstName");
   useEffect(() => {
     axios.get("actors.json").then(res => {
       const newActors = res.data.map(plainActor => new ActorModel(plainActor));
       setActors(newActors);
     });
   }, []);
+
   
   return (
     <Container>
       <Form className="row">
         <Form.Control className="col-8" onChange={(e) => setFilterText(e.target.value)}></Form.Control>
-        <Form.Control value={sortBy}as="select" className="col-4" onChange={(e) => setSortBy(e.target.value)}>
+        <Form.Control value={sortBy} as="select" className="col-4" onChange={(e) => setSortBy(e.target.value)}>
           <option value="firstName">First Name</option>
           <option value="lastName">Last Name</option>
           <option value="age">Age</option>
         </Form.Control>
       </Form>
-      <ActorsCards actors={(filterText.length > 0 ?actors.filter((actor) => actor["firstName"].toLowerCase().includes(filterText)|| actor["lastName"].toLowerCase().includes(filterText)): actors).sort((a,b) => b[sortBy] - a[sortBy])}></ActorsCards>
+      <ActorsCards actors={actors} filterBy = {filterText} sortBy = {sortBy} ></ActorsCards>
     </Container>
   );
 }
--- src/component/ActorsCards.js
+++ src/component/ActorsCards.js
@@ -1,15 +1,22 @@
 import ActorCard from "./ActorCard";
 
-export default function ActorsCards({actors}) {
+export default function ActorsCards(props) {
     let actorsCards;
-    if(actors){
-        actorsCards = actors.map((actor,index) => <ActorCard key={index} actor={actor}/>)
+    let actors = props.actors;
+
+    if (actors) {
+        actors = actors.sort((a, b) => a[props.sortBy] > b[props.sortBy] ?1 : -1);
+        console.log("sortBy: " + props.sortBy + "filter: " + props.filterBy);
+        if (props.filterBy !== "") {
+            actors = actors.filter((actor) => actor["firstName"].toLowerCase().includes(props.filterBy) || actor["lastName"].toLowerCase().includes(props.filterBy));
+        }
+        actorsCards = actors.map((actor, index) => <ActorCard key={index} actor={actor} />)
     }
 
-    return(
+    return (
         <div className="row align-items-center">
             {actorsCards}
         </div>
     )
-    
+
 }
