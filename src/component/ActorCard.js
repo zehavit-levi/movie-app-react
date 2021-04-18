9fbb40e75689ec99ce803360c706e0e630db13bf
Zehavit <levi.zehavit@gmail.com>
2021-04-18 09:39:55 +0300

    add style to actor card

--- src/component/ActorCard.js
+++ src/component/ActorCard.js
@@ -1,16 +1,19 @@
 import { Card } from "react-bootstrap"
+import "./ActorCard.css"
 function ActorCard({ actor }) {
     console.log(actor["image"]);
 
 
     return (
-        <Card className="actor-card col-xs-12 col-sm-6 col-md-4 col-lg-3 align-items-center">
-            <img className="card-img-top" src={actor["image"]} alt="" />
-            <a href={actor.imdbLink} target="blank">
-                <h4 className="card-title">{actor["firstName"] + " " + actor["lastName"]}</h4>
-            </a>
-            <h5>{actor["age"]}</h5>
-        </Card >
+        <div className="card-container col-xs-12 col-sm-6 col-md-4 col-lg-3" >
+            <Card className="actor-card align-items-center">
+                <img className="card-img-top" src={actor["image"]} alt="" />
+                <a href={actor.imdbLink} target="blank" >
+                    <h4 className="card-title">{actor["firstName"] + " " + actor["lastName"]}</h4>
+                </a>
+                <h5>{actor["age"]}</h5>
+            </Card >
+        </div >
     )
 }
 
