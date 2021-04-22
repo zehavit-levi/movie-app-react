b627279a7f91cbc54675db8f2628de89535ac848
Zehavit <levi.zehavit@gmail.com>
2021-04-22 21:19:23 +0300

    add search box

---
+++ src/component/SearchBox/SearchBox.js
@@ -0,0 +1,22 @@
+  
+import React from 'react';
+import { Form, ListGroup } from 'react-bootstrap';
+import './SearchBox.css'
+
+
+function SearchBox({placeholder, searchText, onSearchChange, results, onResultSelected}) {
+    return (
+        <div className="c-searchbox">
+            <Form.Control type="text" placeholder={placeholder} value={searchText} 
+                onChange={e => onSearchChange(e.target.value)}/>
+            <ListGroup className="result-box">
+                {results.map((result, index) => 
+                    <ListGroup.Item action onClick={() => onResultSelected(index)}>
+                        {result}
+                    </ListGroup.Item>)}
+            </ListGroup>
+        </div>
+    );
+}
+
+export default SearchBox;
