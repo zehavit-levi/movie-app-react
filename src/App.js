a760ac562aada9e9c9ce854c2726de665f074f01
Zehavit <levi.zehavit@gmail.com>
2021-04-23 00:11:00 +0300

    render movie page under actors page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import ActorsPage from './pages/ActorsPage';
+import ActorsPage from './pages/ActorsPage/ActorsPage';
+import MoviesPage from './pages/MoviesPage/MoviesPage';
 
 function App() {
   return (
   <div>
-    <ActorsPage></ActorsPage>
+    <ActorsPage/>
+    <MoviesPage/>
   </div>
   );
 }
