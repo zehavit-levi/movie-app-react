8bd8d34d75e5945d324ece7556782ead0a7963c5
Zehavit <levi.zehavit@gmail.com>
2021-04-25 13:49:29 +0300

    add more disgn to movie page

--- src/Pages/MoviesPage/MoviesPage.js
+++ src/Pages/MoviesPage/MoviesPage.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import MovieCard from "../../component/MovieCard/MovieCard";
 import SearchBox from "../../component/SearchBox/SearchBox";
 import MovieModel from "../../model/MovieModel";
+import "./MoviesPage.css"
 
 export default function MoviesPage() {
     const [movies, setMovies] = useState([]);
@@ -26,24 +27,31 @@ export default function MoviesPage() {
         // Adding the movie to the view
         const promise1 = axios.get("https://api.themoviedb.org/3/movie/" + results[resultIndex].id + "?api_key=80aa9e53b86dd9697f92df8248cf222d&language=en-US");
         const promise2 = axios.get("https://api.themoviedb.org/3/movie/" + results[resultIndex].id + "/credits?api_key=80aa9e53b86dd9697f92df8248cf222d&language=en-US");
-        
-        Promise.all([promise1,promise2]).then(responses => {
+
+        Promise.all([promise1, promise2]).then(responses => {
             const directors = responses[1].data.crew.filter(person => person.department === "Directing");
             const mainStars = responses[1].data.cast.filter(person => person.cast_id <= 5);
             const newMovie = new MovieModel(responses[0].data.title, responses[0].data.runtime, responses[0].data.poster_path, directors.map(director => director.name), mainStars.map(star => star.name));
             setMovies(movies.concat(newMovie));
         });
-      
-     
+
+
         setResults([]);
         setSearchText("");
     }
     return (
-        <Container>
-            <SearchBox placeholder="Enter movie for search..." searchText={searchText} onSearchChange={handleSearchChange} results={results.map(result => result.title)} onResultSelected={addMovie} />
+        <Container className="movie-page">
+            <Form className="header-movie-page" >
+                <SearchBox
+                placeholder="Enter movie for search..." 
+                searchText={searchText} 
+                onSearchChange={handleSearchChange} 
+                results={results.map(result => result.title)} 
+                onResultSelected={addMovie} />
+            </Form>
             <Row>
-                    {movies.map((movie,index) => <Col key={index} lg={3} md={6}><MovieCard key={index} movie={movie}/></Col>)}
-                </Row>
+                {movies.map((movie, index) => <Col key={index} lg={3} md={6}><MovieCard key={index} movie={movie} /></Col>)}
+            </Row>
         </Container>
     )
 }
