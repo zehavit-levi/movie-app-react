8d32f4c41905f3941c55692f027978185e5b62b2
Zehavit <levi.zehavit@gmail.com>
2021-04-25 13:45:29 +0300

    add router to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import ActorsPage from './pages/ActorsPage/ActorsPage';
 import MoviesPage from './pages/MoviesPage/MoviesPage';
+import HomePage from './pages/HomePage/HomePage';
+import Switch from 'react-bootstrap/esm/Switch';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { HashRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-  <div>
-    <ActorsPage/>
-    <MoviesPage/>
-  </div>
+    <Container>
+      <Navbar>
+        <Navbar.Brand href="#/">Home Page</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#/movies">movies</Nav.Link>
+          <Nav.Link href="#/actors">actors</Nav.Link>
+        </Nav>
+      </Navbar>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/"><HomePage /></Route>
+          <Route exact path="/movies"><MoviesPage /></Route>
+          <Route exact path="/actors"><ActorsPage /></Route>
+        </Switch>
+      </HashRouter>
+    </Container>
   );
 }
 
