33b9abcfd781a0125c2c3045dca81f474e5b3075
Zehavit <levi.zehavit@gmail.com>
2021-04-25 13:46:04 +0300

    add homepage

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import './HomePage.css'
+export default function HomePage(){
+    return(
+        <Container className="homepage-page">
+            <h1>Wellcome to My home page</h1>
+            <h2>My name is Zehavit levi</h2>
+            <h3>My application build for my react course exercise</h3>
+            <h4>I wish you all the best</h4>
+        </Container>
+    )
+}
